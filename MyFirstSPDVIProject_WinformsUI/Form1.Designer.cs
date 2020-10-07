7ac392ec0f76e251b24073dea78a99f7fcaab530
Jaume Plomer Suau <jaumeplomer@paucasesnovescifp.cat>
2020-10-07 19:54:24 +0200

    Changed the text of the button and form

--- MyFirstSPDVIProject_WinformsUI/Form1.Designer.cs
+++ MyFirstSPDVIProject_WinformsUI/Form1.Designer.cs
@@ -35,10 +35,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(126, 164);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 60);
+            this.button1.Size = new System.Drawing.Size(196, 79);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Boto";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- MyFirstSPDVIProject_WinformsUI/Form1.cs
+++ MyFirstSPDVIProject_WinformsUI/Form1.cs
@@ -16,5 +16,10 @@ namespace MyFirstSPDVIProject_WinformsUI
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
